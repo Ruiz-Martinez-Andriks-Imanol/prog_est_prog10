088ee95dfc429912701d89ecab590f9f267c1701
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 16:19:43 -0500

    prog_est_prog10
    Programa que pide 3 números e imprima el mayor de ellos

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # prog_est_prog10
-Programa que pide 3 números e imprima el mayor de ellos
+Autor: Andriks Imanol Ruiz Mártinez, Fecha: 17/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de condicionales con operadores lógicos en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras</li>
+    <li>printf para mostrar varias variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerias</li>
+    <li>Condicional if-else</li>
+<li>Comentarios para la doumentación interna del programa.</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Descubrir cual de los tres números introducidos es mayor que el otro.
+<br>
+<p>Breve descripción:
+Este programa pide que usemos tres números y que chequemos cual es el mayor. En este programa usaremos tanto el “else if” como el “&&” y con eso tendremos que indicar cuando los valores serán iguales y que valores serán mayores.
+<br>
+</p>
---
+++ prog10.cpp
@@ -0,0 +1,49 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 17/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada 
+Ciclo: 01/2022
+
+Este es un programa de condicionales con operadores lógicos en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  
+    -Variables enteras.
+    -printf para mostrar varias variables.
+    -scanf.
+    -El uso de include para las librerias.
+    -condicional if-else-.
+	-Comentarios para la doumentación interna del programa.
+*/
+#include<stdio.h>
+int main() {
+	//Declaración
+	int a,b,c;
+	//Procesos
+	printf("Introduce el valor de a: ");
+	scanf("%d", &a);
+	printf("Introduce el valor de b: ");
+	scanf("%d", &b);
+	printf("Introduce el valor de c: ");
+	scanf("%d", &c);
+	if(a>b&&a>c){ //Condicionales con operador lógico
+		printf("a es el mayor");
+	}
+	else if(b>a && b>c){
+		printf("b es el mayor");
+	}
+	else if(c>a && c>b){
+		printf("c es el mayor");
+	}
+	else if(c==a && b==c) {
+	printf("Los tres son iguales");	
+	}
+	else if(a==b && a>c){
+		printf("a y b son mayores");
+	}
+	else if(a==c && a>b){
+		printf("a y c son mayores");
+	}
+	else {
+		printf("c y b son mayores");
+	}
+	return 0;
+}
